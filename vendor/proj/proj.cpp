e85efeb0d78d3756dbf7ff6e1e02129f10abb489
Nathan Henrie <nate@n8henrie.com>
2021-11-27 20:09:05 -0700

    Add extra debugging

--- vendor/proj/proj.cpp
+++ vendor/proj/proj.cpp
@@ -2,19 +2,27 @@
 #include <iostream>
 
 void Configuration::set_inherited() {
+    std::cout << "trying set inherited" << std::endl;
     (*this)["foo"] = "bar";
+    std::cout << "done" << std::endl;
 }
 
 void Configuration::print_inherited() {
+    std::cout << "trying print inherited" << std::endl;
     auto found = find ("foo");
     std::cout << found -> second << std::endl;
+    std::cout << "done" << std::endl;
 }
 
 void Configuration::set_composed() {
-    submap["baz"] = "qux";
+    std::cout << "trying set composed" << std::endl;
+    (*this).submap["baz"] = "qux";
+    std::cout << "done" << std::endl;
 }
 
 void Configuration::print_composed() {
+    std::cout << "trying print composed" << std::endl;
     auto found = submap.find("baz");
     std::cout << found -> second << std::endl;
+    std::cout << "done" << std::endl;
 }
