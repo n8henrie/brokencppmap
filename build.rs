f415b738f3ca9b5e34e8e1951a5825df3ef4698c
Nathan Henrie <nate@n8henrie.com>
2021-11-27 20:08:35 -0700

    Try using MaybeUninit

--- build.rs
+++ build.rs
@@ -3,24 +3,27 @@ use std::{env, path::PathBuf};
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() -> Result<()> {
-    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=wrapper.hpp");
     let src = ["vendor/proj/proj.cpp"];
 
     cc::Build::new()
         .cpp(true)
         .files(src.iter())
+        // .include("vendor/proj")
         .flag("-std=c++11")
         // .flag("-shared")
         .compile("proj");
 
     let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .enable_cxx_namespaces()
+        .header("wrapper.hpp")
+        // .enable_cxx_namespaces()
         .clang_arg("-xc++")
         .clang_arg("-std=c++11")
         .clang_arg("-Ivendor/proj")
-        .derive_default(true)
+        // .derive_default(true)
         .allowlist_type("Configuration")
+        // .blocklist_type("std::.*") // prevents inheriting from map?
+        .opaque_type("std::.*")
         .generate()
         .expect("Unable to generate bindings");
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,23 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
-    use super::root::*;
+    // use super::root::*;
+    use super::*;
     type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
     #[test]
     fn test_map() -> Result<()> {
-        let mut config = Configuration::default();
+        dbg!("opening");
+        let mut config = std::mem::MaybeUninit::uninit();
         unsafe {
-            dbg!("before");
-            // config.set_inherited();
-            config.set_composed();
-            dbg!("between");
-            // config.print_inherited();
-            config.print_composed();
+            dbg!("pre-set");
+            // config.set_composed();
+            Configuration_set_inherited(config.as_mut_ptr());
+            let mut config = config.assume_init();
+            dbg!("post-set");
+            // config.print_composed();
+            config.print_inherited();
+            dbg!("done");
         }
         Ok(())
     }
